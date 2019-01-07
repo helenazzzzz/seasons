36e6723bd8ce00ae4611150e0b0d670273f5e84e
Helena Zhang <helenazhang803@gmail.com>
2019-01-07 13:55:32 -0500

    Update seasons.py
    added oop

--- seasons.py
+++ seasons.py
@@ -3,3 +3,44 @@ print("Hello! You are an alien from Neptune, and you want to learn more about Ea
 #selena's section ---
 
 #helena's section ---
+class player():
+    def __init__(self):
+        self.backpack = ['','','']
+        self.lives = 5
+        self.items = 0
+        self.hand = ''
+    def getFromBackpack(self, item):
+        pass
+    
+    def getFromGround(self, item):
+        pass
+
+    def putInBackpack(self, item):
+        pass
+
+    def remove(self, item):
+        pass
+
+    def useItem(self, item):
+        pass
+
+class room():
+    def __init__(self, stage):
+        self.stage = stage
+
+class winter(room):
+    def __init__(self, stage = 'winter'):
+        super().__init__(stage)
+        print('STAGE 4: WINTER')
+
+class game():
+    def __init__(self):
+        me = player()
+        stage4 = winter()
+
+#winter -
+
+def main():
+    newGame = game()
+    
+main()
