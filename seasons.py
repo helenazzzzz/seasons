98b6f27f27ff06ce41ea10f4bad9c3031e3583a0
selena8liu <selena8liu@gmail.com>
2019-01-09 13:17:40 -0500

    Update seasons.py

--- seasons.py
+++ seasons.py
@@ -2,46 +2,6 @@ print("Hello! You are stuck in the house of a Pinterest mom, where each room is 
 print("You have 5 health points, 3 backpack slots, and 1 hand item.")
 print("drop, take, and use")
 
-class player():
-    backpack = ['','','']
-    lives = 5
-    items = 0
-    hand = ''
-    def __init__(self):
-        pass
-    def getFromBackpack(self, item):
-        pass
-    
-    def getFromGround(self, item):
-        pass
-
-    def putInBackpack(self, item):
-        pass
-
-    def remove(self, item):
-        pass
-
-    def useItem(self, item):
-        pass
-    def parseText(text):
-        text = text.lower()
-        if text == "check stats":
-            pass
-        elif "drop " in text:
-            item = text[5:]
-            remove(item)
-        elif "take " in text:
-            item = text[5:]
-            if item in backpack:
-                pass
-        elif "use " in text:
-            item = text[4:]
-        else:
-            print("Sorry, we didn't get that. Please try again. The only text commands are 'drop', 'take', and 'use'.")
-    backpack[0] = '?'
-    parseText('take ?')
-
-
 class room():
 
     passed = False
@@ -60,8 +20,7 @@ class room():
         print('{}. Skip Turn'.format(choices+3))
         selection = input()
     def play(self,level):
-        while not self.passed:
-            self.question(scenes[level][0],questionBank[level][0])
+        self.question(scenes[level][0],questionBank[level][0])
 
 class spring(room):
     def startLevel(self):
@@ -89,10 +48,45 @@ class winter(room):
         print('STAGE 4: WINTER')
 
 class game():
-    
+    backpack = ['','','']
+    lives = 5
+    items = 0
+    hand = ''
     def __init__(self):
-        me = player()
         stage1 = spring()
+        self.backpack = ['item','','']
+    def getFromBackpack(self, item):
+        pass
+    
+    def getFromGround(self, item):
+        pass
+
+    def putInBackpack(self, item):
+        pass
+
+    def remove(self, item):
+        pass
+
+    def useItem(self, item):
+        pass
+    def parseText(self,text):
+        text = text.lower()
+        if text == "check stats":
+            pass
+        elif "drop " in text:
+            item = text[5:]
+            remove(item)
+        elif "take " in text:
+            item = text[5:]
+            if item in self.backpack:
+                pass
+        elif "use " in text:
+            item = text[4:]
+        else:
+            print("Sorry, we didn't get that. Please try again. The only text commands are 'drop', 'take', and 'use'.")
+
+    
+        
 questionBank = [
     [
         ['Choose your selection',
@@ -115,5 +109,6 @@ scenes = [
 
 def main():
     newGame = game()
-    
+    print(newGame.backpack[0])
+    newGame.parseText('take ?')
 main()
