50e0e2bf469a36e8cbfbeb660b0d55709d628a83
Helena Zhang <helenazhang803@gmail.com>
2019-01-11 12:54:34 -0500

    Update seasons.py
    reformat

--- seasons.py
+++ seasons.py
@@ -2,46 +2,47 @@ print("Hello! You are stuck in the house of a Pinterest mom, where each room is 
 print("You have 5 health points, 3 backpack slots, and 1 hand item.")
 print("drop, take, and use")
 
-class room():
-    
-    stage = ''
-    level = ''
-    passed = False
-    def __init__(self, stage, level):
-        self.stage = stage
-        self.level = level
-        print('STAGE {}: {}'.format(self.level+1, self.stage))
-        self.play()
-    def question(self,scene,qSet):
-        choices = len(qSet)
-        print(scene)
-        print(qSet[0])
-        for i in range(1,choices):
-            print(qSet[i])
-        print('B. Backpack and Stats')
+class spring():
+
+    passed = ''
+    def __init__(self):
+        passed = False
+        self.Q1()
+    def Q1(self):
+        print('''Choose your selection:
+              R. Explore the River
+              F. Explore the Forest
+              G. Explore the Garden
+              ''')
         selection = input()
         game.parseText(game,selection)
-    def play(self):
-        while not self.passed:
-            self.question(scenes[self.level][0],questionBank[self.level][0])
+    def Q2(self):
+        pass
+    def Q3(self):
+        pass
+    def Q4(self):
+        pass
+    def Q5(self):
+        pass
+    def Q6(self):
+        pass
 
 class game():
-    backpack = []
+    backpack = ['','','']
     lives = 5
+    items = 0
     hand = ''
+    location = 0
     level = 0
     places = [{'r':1, 'f':2, 'g':3},
               {'s':1, 'o':2, 'b':3},
               {'p':1, 'l':2, 'x':3},
-              {'m':1, 'i':2, 'c':3}]
+              {'m':1, 'i':2, 'c':3},]
     location = ''
     def __init__(self):
         self.backpack = ['','','']
         self.location = 'default_0'
-        stage1 = room('SPRING',0)
-        stage2 = room('SUMMER',1)
-        stage3 = room('FALL',2)
-        stage4 = room('WINTER',3)
+        stage1 = spring()
         
     def getFromBackpack(self, item):
         pass
@@ -59,41 +60,24 @@ class game():
         pass
     def parseText(self,text):
         text = text.lower()
-        if text == "b":
-            print("lives: " + lives)
-            print("item in hand: " + hand)
-            print("backpack: " + backpack)
-        elif text in places[level] and text != location:
-            location = text
-            print("You have now changed locations. " + location)
-        elif text in places[level]:
-            print("You are already there. " + location)
+        if text == "check stats":
+            pass
+        elif "drop " in text:
+            item = text[5:]
+            remove(item)
+        elif "take " in text:
+            item = text[5:]
+            if item in self.backpack:
+                pass
+        elif "use " in text:
+            item = text[4:]
         else:
             print("Sorry, we didn't get that. Please try again. The only text commands are 'drop', 'take', and 'use'.")
 
-    
-        
-questionBank = [
-    [
-        ['Choose your selection',
-         'R. Explore the River',
-         'F. Explore the Forest',
-         'G. Explore the Garden'],
-        ],
-    [
-        ],
-    [
-        ],
-    [
-        ]
-    ]
-scenes = [
-    [
-       '' 
-        ]
-    ]
 
 def main():
     newGame = game()
     print(newGame.backpack[0])
+    newGame.parseText('take ?')
 main()
+
