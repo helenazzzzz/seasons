a712c447027a2e1ce1f39925d58c0c2dbe20bb0d
Helena Zhang <helenazhang803@gmail.com>
2019-01-14 13:26:54 -0500

    Update seasons.py
    some changes

--- seasons.py
+++ seasons.py
@@ -9,11 +9,11 @@ class spring():
     location = ''
     passed = ''
     def __init__(self):
-        current_game = newGame
         self.passed = False
         self.location = 0
         print('The first door leads you into the Spring Room. You are scared and lost, but at least the weather is nice and the birds are chirping.') 
         self.Q1()
+        
     def Q1(self):
         print('''\nChoose your selection:
               R. Explore the River
@@ -22,53 +22,59 @@ class spring():
               B. Backpack / Check Stats
               ''')
         selection = input()
-        choice = game.parseText(game,selection)
+        choice = game.parseText(newGame,selection)
         if choice == 'r':
             self.Q2()
         elif choice == 'f':
             self.Q3()
         elif choice == 'g':
-            self.Q4()
-        else:
-            self.Q1()
-
-
+            self.Q4(0)
+        elif choice != 'b':
+            print('Invalid choice')
+        self.Q1()
+            
     def Q2(self):
-    	print('\nYou are at the River') 
+        print('\nYou are at the Riverbank') 
         print('''Choose your selection:
-                F. Explore the Forest
-                G. Explore the Garden
-                B. Backpack / Check Stats
-                ''')
+              F. Explore the Forest
+              G. Explore the Garden
+              A. Go in the River
+              W. Walk Around
+              B. Backpack / Check Stats
+              ''')
         selection = input()
-        choice = game.parseText(game,selection)
+        choice = game.parseText(newGame,selection)
         if choice == 'f':
             self.Q3()
         elif choice == 'g':
-            self.Q4()
+            self.Q4(0)
         elif choice == 'w':
             self.Q5()
         elif choice == 'a':
             self.Q5()
-        else:
-            self.Q2()
+        elif choice != 'b':
+            print('Invalid choice')
+        self.Q2()
+        
     def Q3(self):
-    	print('\nYou are in the Forest')
-    	print('''Choose your selection:
+        print('\nYou are in the Forest')
+        print('''Choose your selection:
               R. Explore the River
               G. Explore the Garden
+              W. Walk Around
               B. Backpack / Check Stats
               ''')
         selection = input()
-        choice = game.parseText(game,selection)
+        choice = game.parseText(newGame,selection)
         if choice == 'r':
             self.Q2()
         elif choice == 'g':
-            self.Q4()
-        else:
-            self.Q3()
-    def Q4(self,e):
+            self.Q4(0)
+        elif choice != 'b':
+            print('Invalid choice')
+        self.Q3()
         
+    def Q4(self,e):
         event = random.random()
         if event < 0.1:
             newGame.lives -= 1
@@ -84,26 +90,63 @@ class spring():
         print('''Choose your selection:
               R. Explore the River
               F. Explore the Forest
+              W. Wander around
               B. Backpack / Check Stats
               ''')
         selection = input()
-        choice = game.parseText(game,selection)
+        choice = game.parseText(newGame,selection)
         if choice == 'r':
             self.Q2()
         elif choice == 'f':
             self.Q3()
-        else:
-            self.Q4()
+        elif choice != 'b':
+            print('Invalid choice')
+        self.Q4(event)
+            
     def Q5(self):
-        pass
+        print('''Choose your selection:
+              L. Leave the River
+              A. Attempt to fish
+              B. Backpack / Check Stats
+              ''')
+        selection = input()
+        choice = game.parseText(newGame,selection)
+        if choice == 'l':
+            self.Q2()
+        elif choice == 'a':
+            event = random.random()
+            if (newGame.hand == 'fishing rod' and event < 0.5) or event < 0.2:
+                print("You've found a fish!!!!")
+                self.Q7()
+        
+                
+        
     def Q6(self):
-        pass
+        print('''Choose your selection:
+              O. Open Chest
+              I. Ignore
+              B. Backpack / Check Stats
+              ''')
+        selection = input()
+        choice = game.parseText(newGame,selection)
+        if choice == 'o':
+            pass
+        else:
+            return
+        
+    def Q7(self):
+        print('''Choose your selection:
+              P. Pick Up
+              I. Ignore
+              B. Backpack / Check Stats
+              ''')
+    
 
 class game():
-    backpack = ['backpack']
+    backpack = []
     lives = 5
     items = 0
-    hand = 'hand'
+    hand = ''
     level = 0
     places = [{'r':1, 'f':2, 'g':3},
               {'s':1, 'o':2, 'b':3},
@@ -114,7 +157,7 @@ class game():
         self.location = 'default_0'
     def start(self):
         stage1 = spring()
-	def getFromBackpack(self, item):
+    def getFromBackpack(self, item):
         if item in self.backpack:
             self.backpack.remove(item)
             self.backpack.append(self.hand)
@@ -145,12 +188,9 @@ class game():
             self.location = text
             print("You have now changed locations. " + self.location)
             return self.location
-        else:
-            print("Sorry, we didn't get that. Please try again.")
+        
 
 newGame = game()
 newGame.start()
-
-
 
 
